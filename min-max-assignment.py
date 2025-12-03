1ee5a8c29ed1f57e0e1a8d4ba998a5206994ca13
ank-singhal <ankit.singhal@frequency.com>
2025-12-03 16:09:24 +0530

    week 2, assignment 3

---
+++ min-max-assignment.py
@@ -0,0 +1,14 @@
+numbers = [1, 32, 63, 14, 5, 26, 79, 8, 59, 10]
+print(min(numbers))
+print(max(numbers))
+
+setn = {5, 10, 3, 15, 2, 20}
+print(min(setn))
+print(max(setn))
+
+def tuple_of_word_by_length(words):
+    output = min(words, key=len), max(words, key=len);
+    return output
+
+words = ["apple", "banana", "kiwi", "grapefruit", "orange"]
+print(tuple_of_word_by_length(words))
